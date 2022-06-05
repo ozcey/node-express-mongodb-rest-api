41acd2f82848cd3bd6bb2eb50224e61a7a49d473
Oz <ozcancey20@gmail.com>
2022-06-05 14:54:04 -0400

    configured server

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const mongoose = require('mongoose');
+const port  = 3000;
 require('dotenv').config();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS');
+    next();
+});
+
+app.get('/', (req, res) => {
+    res.json({info: 'Node JS, Express JS and MongoDB Backend Rest API'});
+});
+
+app.listen(port, () => {
+    console.log(`App running on port ${3000}`);
+})
