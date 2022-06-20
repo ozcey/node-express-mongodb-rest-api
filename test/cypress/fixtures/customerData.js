7c49badc72996b13e53c24c04322e29ec453f8dd
Oz <ozcancey20@gmail.com>
2022-06-20 00:07:59 -0400

    modified tests

--- test/cypress/fixtures/customerData.js
+++ test/cypress/fixtures/customerData.js
@@ -24,29 +24,3 @@ module.exports = {
           }
     }
 }
-
-// export const customer = {
-//     "name": "Jim Halpert",
-//     "phone": "123-345-0000",
-//     "email": "jimh@gmail.com",
-//     "password": "password",
-//     "address": {
-//         "street": "123 Oak St",
-//         "city": "Columbia",
-//         "state": "MD",
-//         "zipcode": "12345"
-//     }
-// };
-
-// export const updatedCustomer = {
-//     "name": "Jim K. Halpert",
-//     "phone": "123-345-1234",
-//     "email": "jimh@gmail.com",
-//     "password": "12345678",
-//     "address": {
-//         "street": "123 Elm Dr",
-//         "city": "Atlanta",
-//         "state": "GA",
-//         "zipcode": "12345"
-//     }
-// };
--- test/cypress/fixtures/purchaseData.js
+++ test/cypress/fixtures/purchaseData.js
@@ -16,21 +16,3 @@ module.exports = {
           ]
     }
 }
-
-// export const purchase = {
-//     "discountCode": "ABC",
-//     "items": [
-//         "62a0c00a6fc91470efd439fd",
-//         "62a0cb418994b65b5c742747"
-//     ]
-// };
-
-// export const updatedPurchase = {
-//     "date": "6/18/2022 11:11:02 PM",
-//     "total": 165.000,
-//     "discountCode": "DEF",
-//     "items": [
-//         "62a0c00a6fc91470efd439fd",
-//         "62a0cb418994b65b5c742747"
-//     ]
-// };
