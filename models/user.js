e37dc2be6bda3a465b9358c81f2d440ac43f3b46
Oz <ozcancey20@gmail.com>
2022-06-24 22:17:35 -0400

    fixed minor issues

--- models/user.js
+++ models/user.js
@@ -5,8 +5,7 @@ const userSchema = mongoose.Schema({
     name: {
         type: String,
         required: [true, 'Required'],
-        minLength: [2, 'Must be at least 2 characters long, got {VALUE}'],
-        // maxLength: [50, 'Must be at most 50 characters long, got {VALUE}']
+        minLength: [2, 'Must be at least 2 characters long, got {VALUE}']
     },
     email: {
         type: String,
@@ -16,8 +15,7 @@ const userSchema = mongoose.Schema({
     password: {
         type: String,
         required: [true, 'Required'],
-        minLength: [8, 'Must be at least 8 characters long, got {VALUE}'],
-        // maxLength: [25, 'Must be at most 25 characters long, got {VALUE}']
+        minLength: [8, 'Must be at least 8 characters long, got {VALUE}']
     },
     roles: {
         type: Array,
